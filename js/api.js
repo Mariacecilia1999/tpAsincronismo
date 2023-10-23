20930b167537fc972698aa515d6cc60fd057d7de
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2023-10-23 13:55:15 -0300

    Adding base url and showing raw data

---
+++ js/api.js
@@ -0,0 +1,5 @@
+const  urlJobs = 'https://640d6a8f1a18a5db837656bc.mockapi.io/api/jobs'
+
+fetch(urlJobs)
+.then(response => response.json())
+.then(data => showJobs(data))
---
+++ js/main.js
@@ -0,0 +1,11 @@
+const $ = selector => document.querySelector(selector)
+const showJobs = (data) =>{
+   for(const {name, image, description, location,seniority,benefits,salary, long_term, languages, id} of data){
+      $('.allJobs').innerHTML += `
+      <div class='cardJob'>
+         <h2>${name}</h2>
+         <p>${description}</p>
+         <a>${languages}</a>
+      </div>`
+   }
+}
